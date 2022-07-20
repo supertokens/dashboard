85c5bfc80955530db48f27a84f607f975402c750
Jay Mistry <jay@supertokens.com>
2022-07-20 16:16:48 +0530

    cleanup app.tsx and create dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,24 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import './App.css';
+import { getDashboardAppBasePath } from './utils';
 import Auth from './ui/pages/auth/Auth';
-import { getDashboardAppBasePath, getImageUrl } from './utils';
+import AuthWrapper from "./ui/components/authWrapper";
+import Dashboard from "./ui/pages/dashboard/Dashboard";
 
 // This is to make sure that images are packed in the build folder
 import "./images";
-import AuthWrapper from "./ui/components/authWrapper";
-
-function Home() {
-  return (
-    <AuthWrapper>
-      <div className="App">
-        <header className="App-header">
-          <img src={getImageUrl("logo.svg")} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </AuthWrapper>
-  );
-}
 
 function App() {
   return (
     <Router basename={getDashboardAppBasePath()}>
       <Routes>
         <Route path='/auth' element={<Auth />}/>
-        <Route path='/' element={<Home/>} />
+        <Route path='/' element={(
+          <AuthWrapper>
+            <Dashboard />
+          </AuthWrapper>
+        )} />
       </Routes>
     </Router>
   );
---
+++ src/ui/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+
+  return (
+    <div>
+      dashboard component
+    </div>
+  );
+}
+
+export default Dashboard;
