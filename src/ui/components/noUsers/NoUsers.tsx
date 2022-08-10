413afe367e63ecc41ff59a172c6eb8c0575a9e7d
shulhasupertokens <shulha@supertokens.com>
2022-08-10 13:24:47 +0700

    add sentence to no users component

--- src/ui/components/noUsers/NoUsers.tsx
+++ src/ui/components/noUsers/NoUsers.tsx
@@ -6,15 +6,14 @@ import "./NoUsers.css";
 
 const NoUsers = () => {
   return (
-    <div className="no-users">
-      <img
-        src={getImageUrl("no-users-graphic.svg")}
-        alt="Orange and blue users"
-        className="no-users-image"
-      />
+    <div className='no-users'>
+      <img src={getImageUrl('no-users-graphic.svg')} alt='Orange and blue users' className='no-users-image' />
 
-      <p className="no-users-title">Currently, you don't have any users</p>
-      <p className="no-users-subtitle text-small">Once added, all users will be found here</p>
+      <p className='no-users-title'>Currently, you don't have any users</p>
+      <p className='no-users-subtitle text-small'>Once added, all users will be found here</p>
+      <p className='no-users-subtitle text-small'>
+        Users that logged only using the Supertokens' Session Recipe will not be displayed.
+      </p>
     </div>
   )
 };
