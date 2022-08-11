05d37740b99dfe9708cf682c5b71975cc0591fc0
sulha199 <sulha199@gmail.com>
2022-08-11 13:57:02 +0700

    add the required images

--- src/images.ts
+++ src/images.ts
@@ -8,3 +8,6 @@ import "./assets/form-field-error-icon.svg";
 import "./assets/no-users-graphic.svg";
 import "./assets/chevron-left.svg";
 import "./assets/chevron-right.svg";
+import "./assets/right_arrow_icon.svg"
+import "./assets/star_sparkle_stars_sparkles_icon.svg"
+import "./assets/supertokens.svg"
