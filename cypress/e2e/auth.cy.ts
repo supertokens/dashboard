c642558685ed2acf36cd83359703977cada62ab0
sulha199 <sulha199@gmail.com>
2022-08-22 13:02:41 +0700

    update test

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -4,32 +4,38 @@ import { getAppUrl } from "./utils"
 
 const pageUrl = getAppUrl(PATH_USERS)
 
-describe('empty spec', () => {
-  it('shows auth form for un-authenticated users and logins succesfully', async () => {
-    cy.visit(`${pageUrl}`)
-    cy.get(`[data-cy="${CY_AUTH_FORM}"]`)
+describe('Auth', () => {
+  beforeEach(() => {
+    cy.clearLocalStorage()
+  })
 
-    cy.intercept(`${getAppUrl(PATH_API_LOGIN)}`).as('login')
+  it('shows auth form for un-authenticated users and logins succesfully', () => {
+    cy.visit(`${pageUrl}`)
+    cy.get(`[data-cy="${CY_AUTH_FORM}"]`).contains('Enter your API Key')
+
+    cy.intercept(`${(PATH_API_LOGIN)}`).as('login')
     cy.get('.page-container > .block-container > .api-key-form > .input-field-container > .text-small').click() 
     cy.get('.page-container > .block-container > .api-key-form > .input-field-container > .text-small').type(DATA_AUTH_KEY)
  
     cy.get('.page-container > .block-container > .api-key-form > .button > span').click()
-    cy.wait('@login').then(() => {
-      cy.contains('User Management')
-    })
+    cy.wait('@login').its('status').should('eq', 200)
+    cy.contains('User Management')
   })
 
   it('shows auth form for un-authenticated users and fails to login', async () => {
-    cy.visit(`${getAppUrl(PATH_USERS)}`)
-    cy.get(`[data-cy="${CY_AUTH_FORM}"]`)
 
-    cy.intercept(`${getAppUrl(PATH_API_LOGIN)}`).as('login')
+    cy.intercept({
+      method: 'POST',
+      url: `**${PATH_API_LOGIN}`
+    }).as('login')
+    cy.visit(`${pageUrl}`)
+    
+    cy.get(`[data-cy="${CY_AUTH_FORM}"]`).contains('Enter your API Key')
     cy.get('.page-container > .block-container > .api-key-form > .input-field-container > .text-small').click() 
-    cy.get('.page-container > .block-container > .api-key-form > .input-field-container > .text-small').type(DATA_AUTH_KEY)
+    cy.get('.page-container > .block-container > .api-key-form > .input-field-container > .text-small').type('sdasdsad')
  
     cy.get('.page-container > .block-container > .api-key-form > .button > span').click()
-    cy.wait('@login').then(() => {
-      cy.contains('User Management')
-    })
+    cy.wait('@login').its('status').should('eq', 401)
+    cy.find(`[data-cy="${CY_AUTH_FORM}"] .api-key-form .text-error`).contains('Invalid API Key')
   })
 })
