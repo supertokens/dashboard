0492d8ac72d9506fea8a70f9777e41f0a67659b8
sulha199 <sulha199@gmail.com>
2022-08-23 16:03:41 +0700

    add copyright on src folders

--- src/ui/components/auth/Auth.tsx
+++ src/ui/components/auth/Auth.tsx
@@ -12,6 +12,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
+
 import React, { useState } from 'react'
 import { StorageKeys, UNAUTHORISED_STATUS } from '../../../constants'
 import { localStorageHandler } from '../../../services/storage'
