2394f1cd9e85258c05e5ffc3a9ec0a49e21e9571
Nemi Shah <nemishah1212@gmail.com>
2023-03-31 12:59:02 +0530

    Run build

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -29,9 +29,13 @@ export function getDashboardAppBasePath(): string {
 }
 
 export function isSearchEnabled(): boolean {
-	const searchFlag = (window as any).isSearchEnabled;
-	if (searchFlag === undefined) return false;
-	return Boolean(searchFlag);
+	const searchFlag: boolean = (window as any).isSearchEnabled;
+
+	if (searchFlag !== undefined) {
+		return searchFlag;
+	}
+
+	return false;
 }
 
 export function getImageUrl(imageName: string): string {
