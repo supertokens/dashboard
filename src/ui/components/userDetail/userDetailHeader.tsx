84852f97d7a525a2ff9317d013dfca68a15009cd
Nemi Shah <nemishah1212@gmail.com>
2023-05-25 15:06:46 +0530

    Fix issue with user initials not rendering correctly when user meta data is not enabled. Fixes #91

--- src/ui/components/userDetail/userDetailHeader.tsx
+++ src/ui/components/userDetail/userDetailHeader.tsx
@@ -26,9 +26,20 @@ type UserDetailBaseProps = {
 
 const getBadgeInitial = ({ user, recipeId }: UserWithRecipeId) => {
 	const { firstName, lastName, email, id } = user;
+
+	let firstnameToUse = "";
+	let lastNameToUse = "";
+
+	if (firstName !== undefined && firstName !== "FEATURE_NOT_ENABLED") {
+		firstnameToUse = firstName;
+	}
+
+	if (lastName !== undefined && lastName !== "FEATURE_NOT_ENABLED") {
+		lastNameToUse = lastName;
+	}
 	// concatting the firstName & lastname to handle
 	// the case if user enters full name in either firstname or last name
-	const fullName = `${firstName ?? ""} ${lastName ?? ""}`.trim();
+	const fullName = `${firstnameToUse ?? ""} ${lastNameToUse ?? ""}`.trim();
 	if (fullName?.length > 0) {
 		const splitFullName = fullName.split(" ");
 		return splitFullName.length > 1 ? `${splitFullName[0][0]}${splitFullName[1][0]}` : splitFullName[0].slice(0, 2);
