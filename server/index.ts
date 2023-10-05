a5f64b24c6d539afd9767efd7a01e84a12699e9d
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2023-10-05 18:41:01 +0530

    reset index file

--- server/index.ts
+++ server/index.ts
@@ -21,6 +21,7 @@ import { errorHandler, middleware } from "supertokens-node/framework/express";
 import Dashboard from "supertokens-node/lib/build/recipe/dashboard/recipe";
 import AccountLinking from "supertokens-node/recipe/accountlinking";
 import EmailPassword from "supertokens-node/recipe/emailpassword";
+import EmailVerification from "supertokens-node/recipe/emailverification";
 import Passwordless from "supertokens-node/recipe/passwordless";
 import Session from "supertokens-node/recipe/session";
 import ThirdParty from "supertokens-node/recipe/thirdparty";
@@ -82,9 +83,9 @@ SuperTokens.init({
 				],
 			},
 		}),
-		// EmailVerification.init({
-		// 	mode: "REQUIRED",
-		// }),
+		EmailVerification.init({
+			mode: "REQUIRED",
+		}),
 		Session.init(),
 		AccountLinking.init(),
 	],
