8226c75ece902094e0bdce7b8eac1b2f86c2a491
Prateek Surana <prateeksurana3255@gmail.com>
2024-02-08 19:56:49 +0530

    Finish core config UI

--- src/ui/components/tenants/tenantDetail/CoreConfigSection.tsx
+++ src/ui/components/tenants/tenantDetail/CoreConfigSection.tsx
@@ -12,11 +12,16 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ReactComponent as InfoIcon } from "../../../../assets/info-icon.svg";
+import { ReactComponent as PlusIcon } from "../../../../assets/plus.svg";
+import { ReactComponent as RightArrow } from "../../../../assets/right_arrow_icon.svg";
+import { ReactComponent as TrashIcon } from "../../../../assets/trash.svg";
+import Button from "../../button";
 import InputField from "../../inputField/InputField";
 import { Toggle } from "../../toggle/Toggle";
 import TooltipContainer from "../../tooltip/tooltip";
+import { useTenantDetailContext } from "./TenantDetailContext";
 import {
 	PanelHeader,
 	PanelHeaderAction,
@@ -26,7 +31,15 @@ import {
 
 export const CoreConfigSection = () => {
 	const [isEditing, setIsEditing] = useState(false);
+	const { tenantInfo } = useTenantDetailContext();
+	const [currentConfig, setCurrentConfig] = useState(tenantInfo.coreConfig);
 	const hasProperties = true;
+
+	// Ensure that the state reflects the latest core config
+	useEffect(() => {
+		setCurrentConfig(tenantInfo.coreConfig);
+	}, [tenantInfo.coreConfig]);
+
 	return (
 		<PanelRoot>
 			<PanelHeader>
@@ -63,33 +76,69 @@ export const CoreConfigSection = () => {
 					</div>
 					<div className="tenant-detail__core-config-table__body">
 						<CoreConfigTableRow
-							label="cookieDomain"
-							value={true}
+							name="email_verification_token_lifetime"
+							value={3600}
+							type="number"
+							onChange={() => null}
+							isEditing={isEditing}
+							tooltip="The domain to set the cookie to. This is useful when you want to share the cookie across subdomains."
+						/>
+						<CoreConfigTableRow
+							name="email_verification_token_lifetime"
+							value={false}
 							type="boolean"
+							onChange={() => null}
+							isEditing={isEditing}
+							tooltip="The domain to set the cookie to. This is useful when you want to share the cookie across subdomains."
+						/>
+						<CoreConfigTableRow
+							name="email_verification_token_lifetime"
+							value={"sdklfj"}
+							type="string"
+							onChange={() => null}
+							isEditing={isEditing}
 							tooltip="The domain to set the cookie to. This is useful when you want to share the cookie across subdomains."
 						/>
 					</div>
 				</div>
 			)}
+			<div className="tenant-detail__core-config-footer">
+				<Button id="create-tenant">
+					<PlusIcon />
+					Add Property
+				</Button>
+
+				{hasProperties && (
+					<a
+						className="tenant-detail__core-config-footer__footer-link"
+						target="_blank"
+						rel="noreferrer noopener"
+						href="https://github.com/supertokens/supertokens-core/blob/master/config.yaml">
+						See all config properties <RightArrow />
+					</a>
+				)}
+			</div>
 		</PanelRoot>
 	);
 };
 
-type CoreConfigTableRow<T> = {
-	label: string;
+type CoreConfigTableRow<T extends string | number | boolean> = {
+	name: string;
 	value: T;
-	type: T extends string ? "string" : T extends number ? "number" : "boolean";
+	type: "string" | "number" | "boolean" | "enum";
+	onChange: (name: string, newValue: T) => void;
+	isEditing: boolean;
 	tooltip?: string;
 };
 
 type CoreConfigTableRowProps = CoreConfigTableRow<string> | CoreConfigTableRow<number> | CoreConfigTableRow<boolean>;
 
-const CoreConfigTableRow = ({ label, value, type, tooltip }: CoreConfigTableRowProps) => {
+const CoreConfigTableRow = ({ name, value, tooltip, type, isEditing }: CoreConfigTableRowProps) => {
 	const [isActive, setActive] = useState(false);
 	return (
 		<div className="tenant-detail__core-config-table__row">
 			<div className="tenant-detail__core-config-table__row__label">
-				{label}
+				{name}
 				{tooltip && (
 					<TooltipContainer tooltip={tooltip}>
 						<InfoIcon />
@@ -97,22 +146,34 @@ const CoreConfigTableRow = ({ label, value, type, tooltip }: CoreConfigTableRowP
 				)}
 			</div>
 			<div className="tenant-detail__core-config-table__row__value">
-				{type === "string" && (
-					<InputField
-						type="text"
-						name={label}
-						handleChange={() => null}
-						disabled
-						value={value}
-					/>
-				)}
-				{type === "boolean" && (
+				{(type === "string" || type === "number") &&
+					(isEditing ? (
+						<InputField
+							type="text"
+							name={name}
+							handleChange={() => null}
+							value={`${value}`}
+						/>
+					) : (
+						<div className="tenant-detail__core-config-table__row__value__text">{value}</div>
+					))}
+				{typeof value === "boolean" && (
 					<Toggle
-						name={label}
-						id={label}
+						name={name}
+						id={name}
 						checked={isActive}
+						disabled={!isEditing}
 						onChange={() => setActive(!isActive)}
 					/>
+				)}
+
+				{isEditing && (
+					<button
+						onClick={() => null}
+						aria-label="Delete Property"
+						className="tenant-detail__core-config-table__row__delete-property">
+						<TrashIcon />
+					</button>
 				)}
 			</div>
 		</div>
