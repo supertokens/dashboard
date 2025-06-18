7f874b3e34c359102575b9d997982741f53f116f
Favour Ohans <fohanekwu@gmail.com>
2025-06-18 13:52:56 +0100

    🚧 WIP: Users list

--- src/ui/pages/usersList/UsersListTest.tsx
+++ src/ui/pages/usersList/UsersListTest.tsx
@@ -16,11 +16,11 @@
 import PageContainer from "../../components/radix/pageContainer";
 import PageHeading from "../../components/radix/pageHeading";
 import Callout from "../../components/radix/callout";
-import { getImageUrl, isUsingDemoConnectionUri } from "../../../utils";
+import { getConnectionUri, getImageUrl, isSearchEnabled, isUsingDemoConnectionUri } from "../../../utils";
 import { Box, Flex, Select, Text, TextField } from "@radix-ui/themes";
 import Paper from "../../components/radix/paper";
 import { ChevronLeftIcon, ChevronRightIcon, MagnifyingGlassIcon, PlusIcon } from "@radix-ui/react-icons";
-import { useState } from "react";
+import { MutableRefObject, useCallback, useEffect, useRef, useState } from "react";
 import IconButton from "../../components/radix/iconButton";
 
 import "./UsersListTest.scss";
@@ -28,8 +28,18 @@ import Button from "../../components/radix/button";
 import Loader from "../../components/radix/loader";
 import DashboardError from "../../components/radix/error";
 import { assertNever } from "../../../utils/assertNever";
-
-const connectionURL = "https://try.supertokens.com/appid-demo-dashboard";
+import { LIST_DEFAULT_LIMIT } from "../../components/usersListTable/UsersListTable";
+import { User } from "./types";
+import useFetchUsersService from "../../../api/users";
+import { useListTenantsService } from "../../../api/tenants";
+import { useTenantsListContext } from "../../contexts/TenantsListContext";
+import useFetchCount from "../../../api/users/count";
+import { getAuthMode } from "../../../utils";
+import { localStorageHandler } from "../../../services/storage";
+import { StorageKeys } from "../../../constants";
+import { package_version } from "../../../version";
+import { useAnalyticsService } from "../../../api/analytics";
+import useFetchSearchTags from "../../../api/search/searchTags";
 
 const RenderDemoCallout = ({ connectionURI }: { connectionURI: string }) => {
 	if (!isUsingDemoConnectionUri(connectionURI)) return null;
@@ -54,7 +64,10 @@ const RenderDemoCallout = ({ connectionURI }: { connectionURI: string }) => {
 	);
 };
 
-const UserListHeader = ({ tenant, setTenant }: { tenant: string; setTenant: (tenant: string) => void }) => {
+const UserListHeader = ({ onTenantChange }: { onTenantChange: () => void }) => {
+	const { getSelectedTenant, setSelectedTenant, tenantsListFromStore } = useTenantsListContext();
+	const selectedTenant = getSelectedTenant();
+
 	return (
 		<Flex
 			justify="between"
@@ -66,23 +79,29 @@ const UserListHeader = ({ tenant, setTenant }: { tenant: string; setTenant: (ten
 				flexGrow="1"
 				gap="2"
 				align="center"
-				maxWidth="550px">
-				<TextField.Root
-					placeholder="Search by email, user ID, or name"
-					size="2"
-					variant="surface"
-					className="users-list__header__search">
-					<TextField.Slot>
-						<MagnifyingGlassIcon
-							height="16"
-							width="16"
-						/>
-					</TextField.Slot>
-				</TextField.Root>
+				maxWidth="600px">
+				{isSearchEnabled() && (
+					<TextField.Root
+						placeholder="Search by email, phone, or provider"
+						size="2"
+						variant="surface"
+						className="users-list__header__search">
+						<TextField.Slot>
+							<MagnifyingGlassIcon
+								height="16"
+								width="16"
+							/>
+						</TextField.Slot>
+					</TextField.Root>
+				)}
+
 				<Select.Root
 					size="2"
-					value={tenant}
-					onValueChange={setTenant}>
+					value={selectedTenant}
+					onValueChange={(value) => {
+						setSelectedTenant(value);
+						onTenantChange();
+					}}>
 					<Select.Trigger
 						variant="surface"
 						className="users-list__header__select">
@@ -100,14 +119,21 @@ const UserListHeader = ({ tenant, setTenant }: { tenant: string; setTenant: (ten
 								size="2"
 								weight="medium"
 								className="users-list__header__select__text--solid">
-								{tenant}
+								{selectedTenant}
 							</Text>
 						</Flex>
 					</Select.Trigger>
-					<Select.Content position="popper">
-						<Select.Item value="light">Light</Select.Item>
-						<Select.Item value="dark">Dark</Select.Item>
-					</Select.Content>
+					{tenantsListFromStore && (
+						<Select.Content position="popper">
+							{tenantsListFromStore.map((tenant) => (
+								<Select.Item
+									key={tenant.tenantId}
+									value={tenant.tenantId}>
+									{tenant.tenantId}
+								</Select.Item>
+							))}
+						</Select.Content>
+					)}
 				</Select.Root>
 				<IconButton
 					size="2"
@@ -273,9 +299,202 @@ const UserListFooter = () => {
 		</Flex>
 	);
 };
+
+type NextPaginationTokenByOffset = Record<number, string | undefined>;
+type UserListPropsReloadRef = MutableRefObject<(() => Promise<void>) | undefined>;
+
+let isAnalyticsFired = false;
+const limit = LIST_DEFAULT_LIMIT;
+
 export default function UsersListPage() {
-	const [state, setState] = useState<"LOADING" | "ERROR" | "SUCCESS">("LOADING");
-	const [tenant, setTenant] = useState("public");
+	const [pageState, setPageState] = useState<"LOADING" | "ERROR" | "SUCCESS">("LOADING");
+	const [count, setCount] = useState<number>();
+	const [users, setUsers] = useState<User[]>([]);
+	const [offset, setOffset] = useState<number>(0);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [errorOffsets, setErrorOffsets] = useState<number[]>([]);
+	const [isSearch, setIsSearch] = useState<boolean>(false);
+	const [showCreateUserDialog, setShowCreateUserDialog] = useState(false);
+	const [paginationTokenByOffset, setPaginationTokenByOffset] = useState<NextPaginationTokenByOffset>({});
+	const [availableTags, setAvailableTags] = useState<string[]>([]);
+
+	const { fetchSearchTags } = useFetchSearchTags();
+	const { fetchUsers } = useFetchUsersService();
+	const { fetchCount } = useFetchCount();
+	const { fetchTenants } = useListTenantsService();
+	const { fireEvent } = useAnalyticsService();
+
+	const { setTenantsListToStore, tenantsListFromStore, getSelectedTenant, setSelectedTenant } =
+		useTenantsListContext();
+	const selectedTenant = getSelectedTenant();
+	const reloadRef: UserListPropsReloadRef = useRef();
+
+	const connectionURI = getConnectionUri();
+
+	const fetchAndSetAvailableTags = async () => {
+		try {
+			const resp = await fetchSearchTags();
+			setAvailableTags(resp?.tags ?? []);
+		} catch (error) {
+			// TODO: gracefully handle error
+		}
+	};
+
+	const insertUsersAtOffset = useCallback(
+		(paramUsers: User[], paramOffset?: number, isSearch?: boolean) => {
+			if (isSearch) {
+				return [...paramUsers];
+			}
+			if (paramOffset === undefined) {
+				return [...users, ...paramUsers];
+			}
+			return [...users.slice(0, paramOffset), ...paramUsers, ...users.slice(paramOffset + limit)];
+		},
+		[users, limit]
+	);
+
+	const getOffsetByPaginationToken = useCallback(
+		(paginationToken?: string) => {
+			if (paginationToken === undefined) {
+				return 0;
+			}
+			const matchedPaginationTokenByOffsetPair = Object.entries(paginationTokenByOffset).find(
+				([_, token]) => paginationToken === token
+			);
+			return matchedPaginationTokenByOffsetPair !== undefined
+				? parseInt(matchedPaginationTokenByOffsetPair[0])
+				: undefined;
+		},
+		[paginationTokenByOffset]
+	);
+
+	const loadUsers = useCallback(
+		async (paginationToken?: string, search?: object) => {
+			let localSearch = false;
+			const paramOffset = getOffsetByPaginationToken(paginationToken) ?? offset;
+			setLoading(true);
+			const nextOffset = paramOffset + limit;
+			let data;
+			const tenantId = getSelectedTenant();
+			if (paginationToken !== undefined) {
+				data = await fetchUsers({ paginationToken }, undefined, tenantId).catch(() => undefined);
+				setIsSearch(false);
+			} else if (search === undefined || Object.keys(search).length === 0) {
+				data = await fetchUsers(undefined, undefined, tenantId).catch(() => undefined);
+				setIsSearch(false);
+			} else {
+				data = await fetchUsers({ limit: 1000 }, search, tenantId).catch(() => undefined);
+				setIsSearch(true);
+				localSearch = true;
+			}
+			if (data) {
+				// store the users and pagination token
+				const { users: responseUsers, nextPaginationToken } = data;
+				if (localSearch) {
+					setUsers(responseUsers);
+				} else {
+					setUsers(insertUsersAtOffset(responseUsers, paramOffset));
+				}
+				setPaginationTokenByOffset({ ...paginationTokenByOffset, [nextOffset]: nextPaginationToken });
+				setErrorOffsets(errorOffsets.filter((item) => item !== nextOffset));
+			} else {
+				setErrorOffsets([paramOffset]);
+			}
+			setLoading(false);
+			setOffset(paramOffset);
+		},
+		[offset, errorOffsets, limit, paginationTokenByOffset, insertUsersAtOffset, getOffsetByPaginationToken]
+	);
+
+	const fireAnalyticsEvent = async () => {
+		if (isAnalyticsFired) {
+			return;
+		}
+
+		isAnalyticsFired = true;
+
+		try {
+			let email: string | undefined = "apikey@example.com";
+
+			if (getAuthMode() === "email-password") {
+				email = localStorageHandler.getItem(StorageKeys.EMAIL);
+			}
+
+			await fireEvent({
+				email,
+				dashboardVersion: package_version,
+			});
+		} catch (_) {
+			// ignored
+		}
+	};
+
+	const fetchAndSetCurrentTenant = async () => {
+		const result = await fetchTenants();
+		if (!result || !Array.isArray(result?.tenants) || result.tenants.length === 0) {
+			return;
+		}
+
+		setTenantsListToStore(result.tenants);
+
+		const tenantInStorage = getSelectedTenant();
+		let tenantIdToUse: string | undefined;
+
+		if (tenantInStorage === undefined) {
+			tenantIdToUse = result.tenants[0].tenantId;
+			setSelectedTenant(tenantIdToUse);
+		} else {
+			const filteredTenants = result.tenants.filter((t) => t.tenantId === tenantInStorage);
+			if (filteredTenants.length === 0) {
+				tenantIdToUse = result.tenants[0].tenantId;
+				setSelectedTenant(tenantIdToUse);
+			} else {
+				tenantIdToUse = filteredTenants[0].tenantId;
+				setSelectedTenant(tenantIdToUse);
+			}
+		}
+	};
+
+	const loadCount = async () => {
+		const tenantId = getSelectedTenant();
+		const [countResult] = await Promise.all([fetchCount(tenantId).catch(() => undefined), loadUsers()]);
+		if (countResult) {
+			setCount(countResult.count);
+		}
+	};
+
+	const loadOffset = useCallback(
+		async (offset: number) => {
+			await loadUsers(paginationTokenByOffset[offset]);
+		},
+		[paginationTokenByOffset, loadUsers]
+	);
+
+	const onMount = async () => {
+		try {
+			setPageState("LOADING");
+			await fetchAndSetCurrentTenant();
+			await loadCount();
+			await fireAnalyticsEvent();
+			setPageState("SUCCESS");
+		} catch (error) {
+			setPageState("ERROR");
+		}
+	};
+
+	useEffect(() => {
+		void onMount();
+	}, []);
+
+	useEffect(() => {
+		if (reloadRef !== undefined) {
+			reloadRef.current = () => loadOffset(offset);
+		}
+	}, [reloadRef, loadOffset, offset]);
+
+	const onEmailChanged = async () => {
+		await loadOffset(offset);
+	};
 
 	return (
 		<PageContainer>
@@ -284,9 +503,9 @@ export default function UsersListPage() {
 				subtitle="One place to manage all your users, revoke access and edit information according to your needs."
 			/>
 			<div className="users-list">
-				<RenderDemoCallout connectionURI={connectionURL} />
+				<RenderDemoCallout connectionURI={connectionURI} />
 				{(() => {
-					switch (state) {
+					switch (pageState) {
 						case "LOADING":
 							return <Loader type="list" />;
 						case "ERROR":
@@ -294,16 +513,13 @@ export default function UsersListPage() {
 						case "SUCCESS":
 							return (
 								<Paper>
-									<UserListHeader
-										tenant={tenant}
-										setTenant={setTenant}
-									/>
+									<UserListHeader onTenantChange={() => void loadCount()} />
 									<UserListTable />
 									<UserListFooter />
 								</Paper>
 							);
 						default:
-							assertNever(state);
+							assertNever(pageState);
 					}
 				})()}
 			</div>
